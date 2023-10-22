a49eaf49fd8fcf17ae40abb43840c75b7bde012b
Ndro <ndro.lozano@gmail.com>
2023-10-23 01:26:49 +0200

    feat: add prefix

---
+++ .config/nvim/lua/settings/plugins/which_key.lua
@@ -0,0 +1,15 @@
+local wk = require("which-key")
+
+wk.register({
+  ["<leader>"] = {
+    ["F"] = {
+      name = "Flutter",
+    },
+    ["b"] = {
+      name = "Buffer",
+    },
+    ["t"] = {
+      name = "ToDo",
+    },
+  },
+})
