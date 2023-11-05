b83afc45a867893f0af2c86887cd795f2b884d3e
Ndro <ndro.lozano@gmail.com>
2023-11-05 20:10:49 +0100

    asd

--- sync.sh
+++ sync.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #
 
-
+#sudo pacman -S --needed base-devel git
+#git clone https://aur.archlinux.org/paru.git
+#cd paru
+#makepkg -si
+#cd ..
 
 
 echo "··································································";
