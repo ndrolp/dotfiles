6cc87f0803157cac60f98fdff1f1590b65c9b87b
Ndro <ndro.lozano@gmail.com>
2023-11-07 21:40:49 +0100

    feat: dmenu web search focus on browser

--- scripts/dmenu/websearch.sh
+++ scripts/dmenu/websearch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+BROWSER=$(xdg-settings get default-web-browser)
 QUERY=$(printf "" | dmenu -p " 󰖟  " -fn "Hasklug Nerd Font-8" -h 27)
 CON=$(echo $QUERY | tr " " "+" )
 
@@ -6,4 +7,6 @@ CON=$(echo $QUERY | tr " " "+" )
 if [[ $(echo $CON | wc -c) -gt 1 ]]
 then
     xdg-open https://www.google.com/search?q=$CON
+    wmctrl -a CHROME
+    wmctrl -a FIREFOX
 fi
