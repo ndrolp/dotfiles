6faeab026d9932bd760582d321a1bd1210c6313c
Ndro <ndro.lozano@gmail.com>
2023-11-12 16:32:51 +0100

    style: make bookmarks on websearch command

--- scripts/dmenu/websearch.sh
+++ scripts/dmenu/websearch.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+
+GITHUB=" Github"
+YOUTUBE=" Youtube"
+NERD_FONT=" NerdFont"
+
 BROWSER=$(xdg-settings get default-web-browser)
-QUERY=$(printf "" | dmenu -p " 󰖟  " -fn "Hasklug Nerd Font-8" -h 27)
+
+QUERY=$(echo -e "\
+${YOUTUBE}\n\
+${GITHUB}\n\
+${NERD_FONT}\
+" | dmenu -p "   " -fn "Hasklug Nerd Font-8" -h 27 -i)
+
 CON=$(echo $QUERY | tr " " "+" )
 
 
 if [[ $(echo $CON | wc -c) -gt 1 ]]
 then
-    xdg-open https://www.google.com/search?q=$CON
-    ps aux | grep "Chrome" | grep -v "grep"
-    if [ $? -eq 0 ]; then
-        wmctrl -a CHROME
-    else
-        wmctrl -a FIREFOX
-    fi
+    case $QUERY in 
+    $YOUTUBE ) xdg-open https://youtube.com; ;;
+    $GITHUB ) xdg-open https://github.com/; ;;
+    $NERD_FONT ) xdg-open https://www.nerdfonts.com/cheat-sheet; ;;
+    * ) xdg-open https://www.google.com/search?q=$CON
+    esac
 fi
