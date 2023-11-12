545081351b0a670e4718d8c4cac8091ac09b66e6
Ndro <ndro.lozano@gmail.com>
2023-11-12 16:48:26 +0100

    feat: add bookmark to gmail and animeflv

--- scripts/dmenu/websearch.sh
+++ scripts/dmenu/websearch.sh
@@ -3,13 +3,18 @@
 GITHUB=" Github"
 YOUTUBE=" Youtube"
 NERD_FONT=" NerdFont"
+ANIME_FLV="󰿎 Anime FLV"
+GMAIL="󰊫 Gmail"
 
 BROWSER=$(xdg-settings get default-web-browser)
+
 
 QUERY=$(echo -e "\
 ${YOUTUBE}\n\
 ${GITHUB}\n\
-${NERD_FONT}\
+${NERD_FONT}\n\
+${ANIME_FLV}\n\
+${GMAIL}\n\
 " | dmenu -p "   " -fn "Hasklug Nerd Font-8" -h 27 -i)
 
 CON=$(echo $QUERY | tr " " "+" )
@@ -18,9 +23,11 @@ CON=$(echo $QUERY | tr " " "+" )
 if [[ $(echo $CON | wc -c) -gt 1 ]]
 then
     case $QUERY in 
+    $NERD_FONT ) xdg-open https://www.nerdfonts.com/cheat-sheet; ;;
+    $ANIME_FLV ) xdg-open https://www3.animeflv.net/; ;;
+    $GMAIL ) xdg-open https://mail.google.com/; ;;
     $YOUTUBE ) xdg-open https://youtube.com; ;;
     $GITHUB ) xdg-open https://github.com/; ;;
-    $NERD_FONT ) xdg-open https://www.nerdfonts.com/cheat-sheet; ;;
     * ) xdg-open https://www.google.com/search?q=$CON
     esac
 fi
