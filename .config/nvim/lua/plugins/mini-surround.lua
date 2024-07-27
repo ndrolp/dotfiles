c2a9f9dd2483d3916a79648314978c9672d9070d
Ndrolp <ndro.lozano@gmail.com>
2024-07-27 19:19:26 +0200

    change surround keymaps

--- .config/nvim/lua/plugins/mini-surround.lua
+++ .config/nvim/lua/plugins/mini-surround.lua
@@ -11,13 +11,13 @@ return {
 
         -- Module mappings. Use `''` (empty string) to disable one.
         mappings = {
-            add = "<leader>ca",      -- Add surrounding in Normal and Visual modes
-            delete = "<leader>cd",   -- Delete surrounding
-            find = "<leader>cf",     -- Find surrounding (to the right)
-            find_left = "<leader>cF", -- Find surrounding (to the left)
-            highlight = "<leader>ch", -- Highlight surrounding
-            replace = "<leader>cs",  -- Replace surrounding
-            update_n_lines = "<leader>cn", -- Update `n_lines`
+            add = "<leader>sa",      -- Add surrounding in Normal and Visual modes
+            delete = "<leader>sd",   -- Delete surrounding
+            find = "<leader>sf",     -- Find surrounding (to the right)
+            find_left = "<leader>sF", -- Find surrounding (to the left)
+            highlight = "<leader>sh", -- Highlight surrounding
+            replace = "<leader>sr",  -- Replace surrounding
+            update_n_lines = "<leader>sn", -- Update `n_lines`
 
             suffix_last = "l",       -- Suffix to search with "prev" method
             suffix_next = "n",       -- Suffix to search with "next" method
