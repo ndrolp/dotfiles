cb9c9161bfcc5d660e6e51b23a429a7c6fe69539
Ndrolp <ndro.lozano@gmail.com>
2024-08-17 18:04:49 +0200

    remove autocmd nvim

--- .config/nvim/lua/settings/core/general.lua
+++ .config/nvim/lua/settings/core/general.lua
@@ -28,16 +28,16 @@ vim.o.spelllang = "en_us"
 vim.o.cmdheight = 0
 vim.o.shell = "zsh"
 
-vim.cmd([[
-    augroup filetype_jsx
-        autocmd!
-        autocmd FileType javascript set filetype=javascriptreact
-    augroup end
-]])
+-- vim.cmd([[
+--     augroup filetype_jsx
+--         autocmd!
+--         autocmd FileType javascript set filetype=javascriptreact
+--     augroup end
+-- ]])
 
-vim.cmd([[
-    augroup filetype_tsx
-        autocmd!
-        autocmd FileType typescript set filetype=typescriptreact
-    augroup end
-]])
+-- vim.cmd([[
+--     augroup filetype_tsx
+--         autocmd!
+--         autocmd FileType typescript set filetype=typescriptreact
+--     augroup end
+-- ]])
