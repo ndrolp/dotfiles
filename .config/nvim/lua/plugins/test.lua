d4d90c8b53ab985cb557db92753cf1ecb247ff14
Ndrolp <ndro.lozano@gmail.com>
2024-08-17 19:50:48 +0200

    add neotest

---
+++ .config/nvim/lua/plugins/test.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "haydenmeade/neotest-jest",
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-jest")({
+                        jestCommand = "npm run test --",
+                        jestConfigFile = "custom.jest.config.ts",
+                        env = { CI = true },
+                        cwd = function(path)
+                            return vim.fn.getcwd()
+                        end,
+                    }),
+                },
+            })
+        end,
+    },
+}
